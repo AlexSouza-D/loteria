d77f64b0991a1efd00812f3a6afb9053dc7ed9cb
AlexSouza-D <alexmecsouza@hotmail.com>
2022-04-04 08:16:11 -0300

    Alteração-Alex

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-var numSort = []
-var numEsco = []
+var numSort = [];
+var numEsco = [];
 
-function sorteio(){
-    var cont = 0
-    numSort = []
-    while(cont<6){
-        let num = Math.random()*60
-        num = Math.ceil(num)
+function sorteio() {
+  var cont = 0;
+  numSort = [];
 
-        if(!numSort.includes(num)){
-            numSort[cont] = num 
-            console.log(numSort)
-            cont++
-        }
-    
+  if (numEsco.length == 6 && numEsco.indexOf(null) == -1) {
+    while (cont < 6) {
+      let num = Math.random() * 60;
+      num = Math.ceil(num);
+      if (!numSort.includes(num)) {
+        numSort[cont] = num;
+        console.log(numSort);
+        cont++;
+      }
     }
-    document.getElementById("sorteados").innerHTML = numSort
-    contAcertos()
+
+    document.getElementById("sorteados").innerHTML = numSort;
+    contAcertos();
+  } else {
+    alert("É necessário digitar 6 numeros antes do sorteio!");
+  }
 }
 
-function getValor(valor, pos){
-    valor  = Number(valor)
-    
-    if(valor<=0 || valor>60){
-        alert("Número inválido, digite um númmero entre 1 e 60")
-        document.getElementById(`num${pos+1}`).value = ""
-    } else if(numEsco.includes(valor)){
-        alert("Número repetido, escolha outro número!")
-        document.getElementById(`num${pos+1}`).value = ""
-    }else{
-        numEsco[pos] = valor
-        console.log(numEsco)
+function getValor(valor, pos) {
+  valor = Number(valor);
+  if (valor <= 0 || valor > 60) {
+    alert("Numero invalido, digite um entre 1 e 60");
+    document.getElementById(`num${pos + 1}`).value = "";
+  } else if (numEsco.includes(valor)) {
+    alert("Numero repetido, escolha um outro numero!");
+    document.getElementById(`num${pos + 1}`).value = "";
+  } else {
+    numEsco[pos] = valor;
+    console.log(numEsco);
+  }
+}
 
+function contAcertos() {
+  var contA = 0;
+  numEsco.forEach(function (value, index) {
+    if (numSort.includes(value)) {
+      contA = contA + 1;
     }
-    
+  });
+  document.getElementById("acertos").innerHTML = contA;
 }
-
-
-function contAcertos(){
-    let contA = 0
-    numEsco.forEach(function(value, index){
-        if(numSort.includes(value)){    
-            contA = contA+1
-        }      
-    })
-
-    document.getElementById("acertos").innerHTML = contA
-}
-
